8bbdfaebd58b08d61fd8ce8a84fb698e8cfd259d
Ekechi Ikenna <luccy559slim@gmail.com>
2016-06-29 10:08:02 +0100

    modifications to base object

--- lib/corelogic/modules/urls.rb
+++ lib/corelogic/modules/urls.rb
@@ -1,7 +1,6 @@
 module Urls
-  BASE_URL = "https://access-api.corelogic.asia"
-  AUTH_PATH = "/access/oauth/token"
-  PROPERTY_PATH = "/bsg-au"
+  AUTH_BASE_URL = "https://access-api.corelogic.asia/access/oauth/token"
+  PROPERTY_BASE_URL = "https://property-sandbox-api.corelogic.asia/bsg-au"
   SEARCH_PATH = "/search"
   STATISTICS_PATH = "/statistics"
   CHARTS_PATH = "/charts"
--- lib/corelogic/objects/authorization.rb
+++ lib/corelogic/objects/authorization.rb
@@ -1,9 +1,9 @@
 require 'corelogic/objects/base.rb'
 
-class Authorization < CorelogicBaseObject
+class CorelogicAuthorization < CorelogicBaseObject
 
   def self.set_token
-    response = RestClient.get "#{Urls::BASE_URL}#{Urls::AUTH_PATH}", client_id:  Corelogic.new.client_id, client_secret: Corelogic.new.client_secret, grant_type: "client_credentials"
+    response = RestClient.get "#{Urls::AUTH_BASE_URL}", client_id:  Corelogic.new.client_id, client_secret: Corelogic.new.client_secret, grant_type: "client_credentials"
     cookies[:cl_access_token] = { value: response["access_token"], expires: Time.now + response["expires_in"] }
     cookies[:cl_access_token]
   end
