3805c5e1bc2a9005dc826980bbb6556f7b88eac7
Ekechi Ikenna <luccy559slim@gmail.com>
2016-07-12 12:18:39 +0100

    suggestion service and spec

--- spec/corelogic_property_spec.rb
+++ spec/corelogic_property_spec.rb
@@ -13,16 +13,23 @@ describe CorelogicProperty do
   def property_detail
     @obj = File.read(File.expand_path("../../spec/support/fixtures/property.json", __FILE__))
 
-    stub_request(:get, "https://property-sandbox-api.corelogic.asia/bsg-au/v1/property12.json?returnFields=site,%20title,%20address,%20attributes,%20legal,%20avmDetailList,%20contactList,%20currentOwnershipList,%20developmentApplicationList,%20externalReferenceList,%20featureList,%20forRentPropertyCampaignList,%20saleList,%20parcelList,%20forRentPropertyCampaignList,%20forSaleAgencyCampaignList").
+    stub_request(:get, "#{Corelogic::Urls::BASE_URL}#{Corelogic::Urls::PROPERTY_PATH}/v1/property12.json?returnFields=site,%20title,%20address,%20attributes,%20legal,%20avmDetailList,%20contactList,%20currentOwnershipList,%20developmentApplicationList,%20externalReferenceList,%20featureList,%20forRentPropertyCampaignList,%20saleList,%20parcelList,%20forRentPropertyCampaignList,%20forSaleAgencyCampaignList").
         with(:headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip, deflate', 'Authorization'=>'Bearer fdi38adsvljas839.a893azQ38700.839238asdnsidfaeZTY', 'Content-Type'=>'application/json', 'User-Agent'=>'Ruby'}).
         to_return(:status => 200, :body => @obj, :headers => {})
   end
 
   def property_search_by_point
     @result = File.read(File.expand_path("../../spec/support/fixtures/not_found.json", __FILE__))
-    stub_request(:get, "https://property-sandbox-api.corelogic.asia/bsg-au/v2/search/point?lat=-33.9166&lon=151.2833").
+    stub_request(:get, "#{Corelogic::Urls::BASE_URL}#{Corelogic::Urls::PROPERTY_PATH}/v2/search/point?lat=-33.9166&lon=151.2833").
         with(:headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip, deflate', 'Authorization'=>'Bearer fdi38adsvljas839.a893azQ38700.839238asdnsidfaeZTY', 'Content-Type'=>'application/json', 'User-Agent'=>'Ruby'}).
         to_return(:status => 200, :body => @result, :headers => {})
+  end
+
+  def suggestion_services
+    @suggestion = File.read(File.expand_path("../../spec/support/fixtures/suggest.json", __FILE__))
+    stub_request(:get, "#{Corelogic::Urls::BASE_URL}#{Corelogic::Urls::PROPERTY_PATH}/v1/suggest.json?includeBodyCorporates=&includeUnits=&limit=&q=NSW&returnSuggestion=&suggestionTypes=address,%20street,%20locality,%20postcode,%20territorialAuthority,%20councilArea,%20state,%20country").
+        with(:headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip, deflate', 'Authorization'=>'Bearer fdi38adsvljas839.a893azQ38700.839238asdnsidfaeZTY', 'Content-Type'=>'application/json', 'User-Agent'=>'Ruby'}).
+        to_return(:status => 200, :body => @suggestion, :headers => {})
   end
 
 
@@ -47,6 +54,18 @@ describe CorelogicProperty do
     it "should return a valid property object for the location point", type: :request do
       expect(CorelogicProperty.search_by_point(151.28330, -33.91660)).not_to be_nil
       expect(CorelogicProperty.search_by_point(151.28330, -33.91660)).to eq(JSON.parse(@result))
+    end
+  end
+
+  context ".suggestion_service" do
+    before(:each) do
+      authenticate
+      suggestion_services
+    end
+
+    it "should return a suggestion object for the query", type: :request do
+      expect(CorelogicProperty.suggestion_service("NSW")).not_to be_nil
+      expect(CorelogicProperty.suggestion_service("NSW")).to eq(JSON.parse(@suggestion))
     end
   end
 end
