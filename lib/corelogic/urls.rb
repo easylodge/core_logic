8a433c462261a2baa2e2952ae875063dcc505b99
Ekechi Ikenna <luccy559slim@gmail.com>
2016-07-20 13:08:25 +0100

    switch back to sandbox endpoints

--- lib/corelogic/urls.rb
+++ lib/corelogic/urls.rb
@@ -1,11 +1,11 @@
 module Corelogic
   module Urls
-    BASE_URL = "https://property-api.corelogic.asia"
+    BASE_URL = "https://property-sandbox-api.corelogic.asia"
     AUTH_BASE_URL = "https://access-api.corelogic.asia/access/oauth/token"
     PROPERTY_PATH = "/bsg-au"
     POINT_SEARCH_PATH = "/v2/search/point"
     SEARCH_PATH = "/search"
-    SEARCH_BASE_URL = "https://search-api.corelogic.asia"
+    SEARCH_BASE_URL = "https://search-sandbox-api.corelogic.asia"
     ADDRESS_MATCH_PATH = "/au/matcher/address"
   end
 end
